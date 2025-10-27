0f07fcb76a6d514aa1d5a60bfdec18c016fa2456
Ciara-W <cweldon@aggies.ncat.edu>
2025-10-27 16:09:04 -0400

    Update project1_starter.py
    First function

--- project1_starter.py
+++ project1_starter.py
@@ -1,26 +1,38 @@
 """
 COMP 163 - Project 1: Character Creator & Saving/Loading
-Name: [Your Name Here]
-Date: [Date]
+Name: Ciara Weldon
+Date: 10-22-2025
 
 AI Usage: [Document any AI assistance used]
 Example: AI helped with file I/O error handling logic in save_character function
 """
 
 def create_character(name, character_class):
+    name = input("Enter characters name")
+    character_class = input("Enter Class:")
+    level = int(input("Enter level"))
+    gold = int(input("Enter Gold:"))
+
+    char = {
+        "Name" : name,
+        "Character Class" : character_class,
+        "Level" : level,
+        "Gold" : gold,
+        "Magic" : magic,
+        "Health" :  health,
+        "Strength" : strength
+        
+    }
     """
     Creates a new character dictionary with calculated stats
     Returns: dictionary with keys: name, class, level, strength, magic, health, gold
     
-    Example:
-    char = create_character("Aria", "Mage")
-    # Should return: {"name": "Aria", "class": "Mage", "level": 1, "strength": 5, "magic": 15, "health": 80, "gold": 100}
-    """
     # TODO: Implement this function
-    # Remember to use calculate_stats() function for stat calculation
+    # Remember to use calculate_stats() function for stat calculation"""
     pass
 
 def calculate_stats(character_class, level):
+    
     """
     Calculates base stats based on class and level
     Returns: tuple of (strength, magic, health)
